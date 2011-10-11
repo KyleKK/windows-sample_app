083626d7bd705e64ba957a82f64ea1477fd5babf
Kyle Kujala-Korpela <kyle.kujala.korpela@gmail.com>
2011-10-11 16:23:04 -0500

    dauh dah duh DUH

--- Gemfile
+++ Gemfile
@@ -12,5 +12,6 @@ group :test do
   gem 'rspec-rails', '2.6.1'
   gem 'webrat', '0.7.1'
   gem 'spork', '0.9.0.rc8'
+  gem 'autotest', '4.4.6'
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+#returns a title based on page
+def title
+	base_title = "Ruby on Rails Tutorial Sample App"
+	if @title.nil?
+		base_title
+	else
+		"#{base_title} | #{@title}"
+	end
+end
+
+
 end
