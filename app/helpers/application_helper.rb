9c04d48f1e19d2e61e33644fd12c6750e4cf2a1a
Kyle Kujala-Korpela <kyle.kujala.korpela@gmail.com>
2011-10-13 10:51:16 -0500

    Finished the first draft of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+	def logo
+         logo = image_tag("logo.png", :alt=>"Sample App", :class=>"round") 
+	end
+
 #returns a title based on page
 def title
 	base_title = "Ruby on Rails Tutorial Sample App"
